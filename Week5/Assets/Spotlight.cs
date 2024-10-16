e046887962edbd67d98ced769c2a08cb93bfda17
DryreL <gokdenizcetintr@gmail.com>
2024-10-16 20:51:05 +0300

    Week 5 project files added (Flashlight)

---
+++ Week5/Assets/Spotlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour
+{
+
+    public Light isik;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isik = this.gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isik.enabled = !isik.enabled;
+        }
+
+    }
+}
